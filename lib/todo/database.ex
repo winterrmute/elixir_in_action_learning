dcf983958e6587d628c890200daf3bca9581d82e
winterrmute <winterrmut3@gmail.com>
2025-09-14 14:44:14 +0200

    finalize chapter 7 before exercise

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -4,15 +4,15 @@ defmodule Todo.Database do
   @db_folder "./persist"
 
   def start do
-    GenServer.start(__MODULE__, nil, name: __MODULE__)
+    GenServer.start(__MODULE__, nil, name: :database_server)
   end
 
   def store(key, data) do
-    GenServer.cast(__MODULE__, {:store, key, data})
+    GenServer.cast(:database_server, {:store, key, data})
   end
 
   def get(key) do
-    GenServer.call(__MODULE__, {:get, key})
+    GenServer.call(:database_server, {:get, key})
   end
 
   @impl GenServer
@@ -21,22 +21,30 @@ defmodule Todo.Database do
     {:ok, nil}
   end
 
+  @impl GenServer
   def handle_cast({:store, key, data}, state) do
-    key
-    |> file_name()
-    |> File.write!(:erlang.term_to_binary(data))
+    spawn(fn ->
+      key
+      |> file_name()
+      |> File.write!(:erlang.term_to_binary(data))
+    end)
 
     {:noreply, state}
   end
 
-  def handle_call({:get, key}, _, state) do
-    data =
-      case File.read(file_name(key)) do
-        {:ok, content} -> :erlang.binary_to_term(content)
-        _ -> nil
-      end
+  @impl GenServer
+  def handle_call({:get, key}, caller, state) do
+    spawn(fn ->
+      data =
+        case File.read(file_name(key)) do
+          {:ok, content} -> :erlang.binary_to_term(content)
+          _ -> nil
+        end
 
-    {:reply, data, state}
+      GenServer.reply(caller, data)
+    end)
+
+    {:noreply, state}
   end
 
   def file_name(key) do
--- lib/todo/list/list.ex
+++ lib/todo/list/list.ex
@@ -3,11 +3,11 @@ defmodule Todo.List do
 
   defstruct next_id: 1, entries: %{}
 
-  defp new(entries \\ []) do
+  def new(entries \\ []) do
     Enum.reduce(entries, %__MODULE__{}, &add_entry(&2, &1))
   end
 
-  defp add_entry(todo_list, entry) do
+  def add_entry(todo_list, entry) do
     entry = Map.put(entry, :id, todo_list.next_id)
 
     new_entries =
@@ -20,16 +20,16 @@ defmodule Todo.List do
     %Todo.List{todo_list | entries: new_entries, next_id: todo_list.next_id + 1}
   end
 
-  defp get_entry_by_id(todo_list, entry_id) do
+  def get_entry_by_id(todo_list, entry_id) do
     MultiDict.get(todo_list.entries, entry_id)
   end
 
-  defp update_entry(todo_list, entry_id, new_date) do
+  def update_entry(todo_list, entry_id, new_date) do
     updated_entries = MultiDict.update(todo_list.entries, entry_id, &Map.put(&1, :date, new_date))
     %Todo.List{todo_list | entries: updated_entries}
   end
 
-  defp remove_entry(todo_list, entry_id) do
+  def remove_entry(todo_list, entry_id) do
     updated_entries = MultiDict.remove(todo_list.entries, entry_id)
     %Todo.List{todo_list | entries: updated_entries}
   end
