87268be03d7d0cdf4db9b9509125b9e5f93a3cda
winterrmute <winterrmut3@gmail.com>
2025-09-20 14:08:11 +0200

    chapter 8 finished

---
+++ chapter8/lib/todo/list/list.ex
@@ -0,0 +1,47 @@
+defmodule Todo.List do
+  use GenServer
+
+  defstruct next_id: 1, entries: %{}
+
+  def new(entries \\ []) do
+    IO.puts("new list creating")
+    Enum.reduce(entries, %__MODULE__{}, &add_entry(&2, &1))
+  end
+
+  def add_entry(%__MODULE__{next_id: next_id} = todo_list, entry) do
+    entry = Map.put(entry, :id, next_id)
+
+    new_entries =
+      Map.put(
+        todo_list.entries,
+        todo_list.next_id,
+        entry
+      )
+
+    %Todo.List{todo_list | entries: new_entries, next_id: next_id + 1}
+  end
+
+  def entries(%__MODULE__{entries: entries}, date) do
+    entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry.title end)
+  end
+
+  def get_entry_by_id(todo_list, entry_id) do
+    MultiDict.get(todo_list.entries, entry_id)
+  end
+
+  def update_entry(
+        %__MODULE__{entries: entries} = todo_list,
+        entry_id,
+        new_date
+      ) do
+    updated_entries = MultiDict.update(entries, entry_id, &Map.put(&1, :date, new_date))
+    %Todo.List{todo_list | entries: updated_entries}
+  end
+
+  def remove_entry(%__MODULE__{entries: entries} = todo_list, entry_id) do
+    updated_entries = MultiDict.remove(entries, entry_id)
+    %Todo.List{todo_list | entries: updated_entries}
+  end
+end
---
+++ chapter8/lib/todo/server/todo_server.ex
@@ -0,0 +1,39 @@
+defmodule Todo.Server do
+  use GenServer
+
+  # Interface functions
+  def start(name) do
+    GenServer.start(__MODULE__, name)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    GenServer.cast(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date \\ nil) do
+    GenServer.call(todo_server, {:entries, date})
+  end
+
+  def handle_continue(:init, {name, nil}) do
+    {:noreply, {name, Todo.Database.get(name) || Todo.List.new()}}
+  end
+
+  # Callback functions
+  def init(name) do
+    {:ok, {name, Todo.Database.get(name) || Todo.List.new()}}
+  end
+
+  def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
+    new_state = Todo.List.add_entry(todo_list, new_entry)
+    Todo.Database.store(name, new_state)
+    {:noreply, {name, new_state}}
+  end
+
+  def handle_call({:entries, nil}, _, {name, todo_list}) do
+    {:reply, todo_list, {name, todo_list}}
+  end
+
+  def handle_call({:entries, date}, _, {name, todo_list}) do
+    {:reply, Todo.List.entries(todo_list, date), {name, todo_list}}
+  end
+end
